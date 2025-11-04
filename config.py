bfd1740fe25961c225f2ea1f06d580c53b9e4f8e
oid85 <oid85@mail.ru>
2025-11-04 17:19:12 +0700

    create scripts

---
+++ config.py
@@ -0,0 +1,7 @@
+deploy_path = 'C:\\Deploy'
+
+athletic_source_path = 'C:\\Repository\\Oid85.Athletic\\Oid85.Athletic'
+athletic_deploy_directory = 'Oid85.FinMarket'
+athletic_exe_file_name = "Oid85.FinMarket.WebHost.exe"
+athletic_service_name = 'Oid85.FinMarket'
+athletic_appsettings_file = 'appsettings.json'
